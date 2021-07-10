2787412c8d0d293e90003ac4a9078e751ef3bf25
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-07-10 15:44:47 +0530

    added 2nd part of sorted 2d array

--- SearchInA2DArray.java
+++ SearchInA2DArray.java
@@ -21,7 +21,7 @@ public class SearchInA2DArray
 
         int target = scn.nextInt();
 
-        boolean isFound = search(matrix,target);
+        boolean isFound = search2(matrix,target);
         System.out.println(isFound);
     }
     public static boolean search1(int[][]arr,int target) {
@@ -74,5 +74,48 @@ public class SearchInA2DArray
         }
         return false;
     }
-    
+
+    //2nd approach
+   public static boolean search2(int[][]arr,int target) {
+       int n = arr.length;
+       int m = arr[0].length;
+
+       int i = 0;
+       int j = n-1;
+
+       while(i <= j)
+       {
+           int mid = (i+j)/2;
+
+           if(arr[mid][0] <= target && arr[mid][m-1] >=target )
+           {
+                 int a=0,b=m-1;
+                 while( a <= b)
+                 {
+                     int mid1 = (a+b)/2;
+                     if(arr[mid][mid1] == target)
+                     {
+                                 return true;
+                     }
+                     else if(arr[mid][mid1] > target)
+                     {
+                            b = mid1 - 1;
+                     }
+                     else{
+                            a = mid1 + 1;
+                     }
+                 }
+           }
+           else if(arr[mid][0] > target)
+           {
+                 j = mid - 1;
+           }
+           else
+           {
+                  i = mid + 1;
+           }
+       }
+       return false;
+   }
+
 }
